c59e25cc98a8076be1c9109139222ea81d2252f9
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-12 15:25:20 +0100

    set up test database that clears after each test

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,5 +1,11 @@
 feature 'view bookmarks' do
   scenario 'User wants to see all bookmarks' do
+    connection = PG.connect(dbname: 'bookmark_manager_test')
+
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.makersacademy.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com');")
+    connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.google.com');")
+
     visit('/bookmarks')
     expect(page).to have_content "http://www.makersacademy.com"
     expect(page).to have_content "http://www.google.com"
