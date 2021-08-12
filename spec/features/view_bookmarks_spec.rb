3d7287f20840abe34717255bdbd671243e070cbe
Michael PS <michael.plasomscott@hotmail.co.uk>
2021-08-12 14:14:51 +0100

    database connected

--- spec/features/view_bookmarks_spec.rb
+++ spec/features/view_bookmarks_spec.rb
@@ -1,8 +1,8 @@
 feature 'view bookmarks' do
   scenario 'User wants to see all bookmarks' do
     visit('/bookmarks')
-    expect(page).to have_content "https://github.com/makersacademy/course/blob/main/pills/ruby_web_project_setup_list.md"
-    expect(page).to have_content "https://github.com/makersacademy/course/blob/main/apprenticeship_module_outlines.md"
-    expect(page).to have_content "https://airtable.com/shrJMn1quurUpejHI/tblU9M74TCpg89oGC"
+    expect(page).to have_content "http://www.makersacademy.com"
+    expect(page).to have_content "http://www.google.com"
+    expect(page).to have_content "http://www.destroyallsoftware.com"
   end
 end
